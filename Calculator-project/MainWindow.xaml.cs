711a5900df5bff7c3d4a57d5c6cb60727c1e3252
Baesar <noju22ez@student.ju.se>
2024-04-01 08:37:56 +0200

    NumBtn_Click clean-up

--- Calculator-project/MainWindow.xaml.cs
+++ Calculator-project/MainWindow.xaml.cs
@@ -38,72 +38,78 @@ namespace Calculator_project
 
         private void NumBtn_Click(object sender, RoutedEventArgs e)
         {
-            String name = ((Button)sender).Name;
-
-            switch(name)
-            {
-                case "OneBtn":
-                    output += "1";
-                    OutputTextBlock.Text = output;
-
-                    break;
-
-                case "TwoBtn":
-                    output += "2";
-                    OutputTextBlock.Text = output;
-
-                    break;
-
-                case "ThreeBtn":
-                    output += "3";
-                    OutputTextBlock.Text = output;
-
-                    break;
-
-                case "FourBtn":
-                    output += "4";
-                    OutputTextBlock.Text = output;
-
-                    break;
-
-                case "FiveBtn":
-                    output += "5";
-                    OutputTextBlock.Text = output;
-
-                    break;
-
-                case "SixBtn":
-                    output += "6";
-                    OutputTextBlock.Text = output;
-
-                    break;
-
-                case "SevenBtn":
-                    output += "7";
-                    OutputTextBlock.Text = output;
-
-                    break;
-
-                case "EightBtn":
-                    output += "8";
-                    OutputTextBlock.Text = output;
-
-                    break;
-
-                case "NineBtn":
-                    output += "9";
-                    OutputTextBlock.Text = output;
-
-                    break;
-
-                case "ZeroBtn":
-                    output += "0";
-                    OutputTextBlock.Text = output;
-
-                    break;
+            output += ((Button)sender).Content;
+            OutputTextBlock.Text = output;
 
 
-            }
+            // ----- Old Code ----- //
+
+            //String name = ((Button)sender).Name;
+
+            //switch(name)
+            //{
+            //    case "OneBtn":
+            //        output += "1";
+            //        OutputTextBlock.Text = output;
+
+            //        break;
+
+            //    case "TwoBtn":
+            //        output += "2";
+            //        OutputTextBlock.Text = output;
+
+            //        break;
+
+            //    case "ThreeBtn":
+            //        output += "3";
+            //        OutputTextBlock.Text = output;
+
+            //        break;
+
+            //    case "FourBtn":
+            //        output += "4";
+            //        OutputTextBlock.Text = output;
+
+            //        break;
+
+            //    case "FiveBtn":
+            //        output += "5";
+            //        OutputTextBlock.Text = output;
+
+            //        break;
+
+            //    case "SixBtn":
+            //        output += "6";
+            //        OutputTextBlock.Text = output;
+
+            //        break;
+
+            //    case "SevenBtn":
+            //        output += "7";
+            //        OutputTextBlock.Text = output;
+
+            //        break;
+
+            //    case "EightBtn":
+            //        output += "8";
+            //        OutputTextBlock.Text = output;
+
+            //        break;
+
+            //    case "NineBtn":
+            //        output += "9";
+            //        OutputTextBlock.Text = output;
+
+            //        break;
+
+            //    case "ZeroBtn":
+            //        output += "0";
+            //        OutputTextBlock.Text = output;
+
+            //        break;
+
+
+            //}
             
         }
 
