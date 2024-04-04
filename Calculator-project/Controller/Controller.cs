a6a4660edcde3efa60d472b2e39f27f4c2786c51
Baesar <noju22ez@student.ju.se>
2024-04-04 16:28:41 +0200

    Revert "added useless comment"
    This reverts commit ee29c11bd9144962eaed1b5a09e707e46c7bb686.

--- Calculator-project/Controller/Controller.cs
+++ Calculator-project/Controller/Controller.cs
@@ -1,7 +1,12 @@
-﻿namespace Calculator_project.Controller
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_project.Controller
 {
     internal class Controller
     {
-        // useless comment
     }
 }
