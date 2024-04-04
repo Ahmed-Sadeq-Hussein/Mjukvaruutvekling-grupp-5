d3433eb07c1f33981e9dd83f301944b3864ca18b
SanaSu1 <susa21ly@student.ju.se>
2024-04-04 14:38:40 +0200

    added gitignore

--- Calculator-project/obj/Debug/net6.0-windows/Calculator-project_gzwz5btp_wpftmp.AssemblyInfo.cs
+++ Calculator-project/obj/Debug/net6.0-windows/Calculator-project_gzwz5btp_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64eb49f86ed850cd846419e5f2d4c2a0352130c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f635a7dfa936c873db23d2073c1824a68c5bc54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
