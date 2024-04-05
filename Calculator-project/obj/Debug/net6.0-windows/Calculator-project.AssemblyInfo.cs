280acb8f59108e1a4fd7336e224c924f12110368
Baesar <noju22ez@student.ju.se>
2024-04-05 16:53:25 +0200

    nothing

--- Calculator-project/obj/Debug/net6.0-windows/Calculator-project.AssemblyInfo.cs
+++ Calculator-project/obj/Debug/net6.0-windows/Calculator-project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d35e94f302e2b9c63d269a4f142f77a7a78791")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
