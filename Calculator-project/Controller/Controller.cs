2e18ffb0623e0e70e913adc7888112a3ac4f6ffe
Baesar <noju22ez@student.ju.se>
2024-04-15 14:05:30 +0200

    nothing kinda

--- Calculator-project/Controller/Controller.cs
+++ Calculator-project/Controller/Controller.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Windows;
 namespace Calculator_project.Controller
 {
-    internal class Controller
+    public class Controller
     {
         //private static Queue<Equation> equationQueue = new Queue<Equation>(10);
 
