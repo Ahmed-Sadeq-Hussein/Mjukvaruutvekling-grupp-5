de64618935f0a3aa9628481edd4a01886126e360
Baesar <noju22ez@student.ju.se>
2024-04-18 16:58:53 +0200

    Added som tests of the SortToTokenList method

--- Calculator-project.nUnitTests/ControllerTests.cs
+++ Calculator-project.nUnitTests/ControllerTests.cs
@@ -1,3 +1,5 @@
+using Calculator.Model;
+
 namespace Calculator_project.nUnitTests
 {
     public class ControllerTests
@@ -11,16 +13,51 @@ namespace Calculator_project.nUnitTests
         }
 
         [Test]
-        public void SortToTokenList_Test()
+        public void SortToTokenList_EqualTest()
         {
             // Assign
+            string expression = "2+0-1.x41.41/0.2^1";
+            List<Token> tokenList = new List<Token>();
 
+            // Act
+            tokenList = controller.SortToTokenList(expression);
+
+            // Assert
+            Assert.AreEqual("2", tokenList[0].ToString());
+            Assert.AreEqual("[SumOperator]", tokenList[1].ToString());
+            Assert.AreEqual("0", tokenList[2].ToString());
+            Assert.AreEqual("[SubtractOperator]", tokenList[3].ToString());
+            Assert.AreEqual("1", tokenList[4].ToString());
+            Assert.AreEqual("[MultiplyOperator]", tokenList[5].ToString());
+            Assert.AreEqual("41,41", tokenList[6].ToString());
+            Assert.AreEqual("[DivideOperator]", tokenList[7].ToString());
+            Assert.AreEqual("0,2", tokenList[8].ToString());
+            Assert.AreEqual("[ExponentiateOperator]", tokenList[9].ToString());
+            Assert.AreEqual("1", tokenList[10].ToString());
+        }
+
+        [Test]
+        public void SortToTokenList_ThrowsExceptionTest1()
+        {
+            // Assign
+            string expression = "2+";
 
             // Act
 
+            // Assert
+            Assert.Throws<Calculator_project.Exceptions.InvalidExpressionException>(() => controller.SortToTokenList(expression));
+        }
+
+        [Test]
+        public void SortToTokenList_ThrowsExceptionTest2()
+        {
+            // Assign
+            string expression = "2+";
+
+            // Act
 
             // Assert
-
+            Assert.Throws<Calculator_project.Exceptions.InvalidExpressionException>(() => controller.SortToTokenList(expression));
         }
 
         [Test]
