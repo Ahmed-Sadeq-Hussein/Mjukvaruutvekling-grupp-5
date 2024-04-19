732c192ac6021201bf24762fed19a17a3a24df30
Baesar <nujo22ez@student.ju.se>
2024-04-19 13:05:07 +0200

    idk

---
+++ Calculator-project.nUnitTests/ModelTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator_project.Model;
+
+namespace Calculator_project.nUnitTests
+{
+    public class ModelTests
+    {
+        [Test]
+        public void SumOperator_EqualTest()
+        {
+            // Assign
+            SumOperator oPerator = new SumOperator();
+            double x = 1;
+            double y = 2;
+
+            // Act
+            double result = oPerator.Compute(x,y);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void SubtractOperator_EqualTest()
+        {
+            // Assign
+            SubtractOperator oPerator = new SubtractOperator();
+            double x = 1;
+            double y = 2;
+
+            // Act
+            double result = oPerator.Compute(x,y);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void MultiplyOperator_EqualTest()
+        {
+            // Assign
+            MultiplyOperator oPerator = new MultiplyOperator();
+            double x = 1;
+            double y = 2;
+
+            // Act
+            double result = oPerator.Compute(x,y);
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void DivideOperator_EqualTest()
+        {
+            // Assign
+            DivideOperator oPerator = new DivideOperator();
+            double x = 1;
+            double y = 2;
+
+            // Act
+            double result = oPerator.Compute(x, y);
+
+            // Assert
+            Assert.AreEqual(0.5, result);
+        }
+
+        [Test]
+        public void ExponentiateOperator_EqualTest()
+        {
+            // Assign
+            ExponentiateOperator oPerator = new ExponentiateOperator();
+            double x = 1;
+            double y = 2;
+                
+            // Act
+            double result = oPerator.Compute(x,y);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+    }
+}
