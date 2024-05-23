be4cd99115fd0a0357e270e92012e3675acb3c3a
Baesar <noju22ez@student.ju.se>
2024-05-23 11:15:57 +0200

    Added the new exception file

---
+++ Calculator-project/Exceptions/ResultsInImaginaryNumberException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Calculator_project.Exceptions
+{
+    internal class ResultsInImaginaryNumberException : Exception
+    {
+        public ResultsInImaginaryNumberException()
+        {
+        }
+
+        public ResultsInImaginaryNumberException(string? message) : base(message)
+        {
+        }
+
+        public ResultsInImaginaryNumberException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ResultsInImaginaryNumberException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public override string Message => $"Expression resulted in an imaginary number";
+    }
+}
