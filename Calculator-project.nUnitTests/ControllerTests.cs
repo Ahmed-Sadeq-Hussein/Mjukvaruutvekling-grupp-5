2e764e80ee32a5a01f1d793d6be000f0e088fe36
Rama Muharam <mura22aw@student.ju.se>
2024-05-25 17:23:02 +0200

    fixed the integration test

--- Calculator-project.nUnitTests/ControllerTests.cs
+++ Calculator-project.nUnitTests/ControllerTests.cs
@@ -564,7 +564,7 @@ namespace Calculator_project.nUnitTests
         {
             // Assign
             string expression = "2 + 3 * (4 - 1)";
-            string expectedOutput = "11";
+            string expectedOutput = "15";
 
             // Act
             string result = controller.CalculateExpression(expression, true);
